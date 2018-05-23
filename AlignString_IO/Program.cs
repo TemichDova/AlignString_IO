589509df6d5836e0ea0ae7cb52d9246c3d91c7d3
TemichDova <pirokar96@mail.ru>
2018-05-23 17:25:46 +0300

    IO String

---
+++ AlignString_IO/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AlignString_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string skip = "";
+            int skipSymbol = 0;
+            List<string> outWords = new List<string>();
+
+            // Путь к файлам
+            string pathIn = @"..\..\..\in.txt";
+            string pathOut = @"..\..\..\out.txt";
+
+            StreamReader _pathIn = new StreamReader(pathIn, Encoding.Default);
+            StreamWriter _pathOut = new StreamWriter(pathOut,false, Encoding.Default);
+
+            // Чтение файла
+            using ( StreamReader sr = _pathIn)
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    if (skipSymbol < s.Length) skipSymbol = s.Length;
+                    outWords.Add(s);
+                    Console.WriteLine(s+" "+ skipSymbol);
+                }
+            }
+
+            //Количество пробелов
+            for (int i = 0; i < skipSymbol; i++)
+            {
+                skip += " ";
+            }
+           
+            // Запись в файл File.CreateText(pathOut)
+            using (StreamWriter sw = _pathOut)
+            {
+                foreach(string n in outWords)
+                {
+                    sw.WriteLine(skip+n);
+                }
+            }
+                Console.ReadLine();
+        }
+        
+    }
+}
---
+++ AlignString_IO/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("AlignString_IO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlignString_IO")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4a3f16db-2874-4933-ba16-e690119b79cc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
