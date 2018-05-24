792d2227b7a323b69bc7bd66eb9d711225c673e9
TemichDova <pirokar96@mail.ru>
2018-05-24 10:11:02 +0300

    add MethodTwo

--- AlignString_IO/Program.cs
+++ AlignString_IO/Program.cs
@@ -9,9 +9,11 @@ namespace AlignString_IO
     {
         static void Main(string[] args)
         {
+            /*
             string skip = "";
             int skipSymbol = 0;
             List<string> outWords = new List<string>();
+            */
 
             // Путь к файлам
             string pathIn = @"..\..\..\in.txt";
@@ -20,8 +22,20 @@ namespace AlignString_IO
             StreamReader _pathIn = new StreamReader(pathIn, Encoding.Default);
             StreamWriter _pathOut = new StreamWriter(pathOut,false, Encoding.Default);
 
+
+            // MethodOne(_pathIn, _pathOut); первый алгоритм
+            MethodTwo(_pathIn, _pathOut);
+
+            Console.ReadLine();
+        }
+        
+        static void MethodOne(StreamReader _pathIn, StreamWriter _pathOut)
+        {
+            string skip = "";
+            int skipSymbol = 0;
+            List<string> outWords = new List<string>();
             // Чтение файла
-            using ( StreamReader sr = _pathIn)
+            using (StreamReader sr = _pathIn)
             {
                 string s = "";
                 while ((s = sr.ReadLine()) != null)
@@ -36,17 +50,60 @@ namespace AlignString_IO
             {
                 skip += " ";
             }
-           
+
             // Запись в файл File.CreateText(pathOut)
             using (StreamWriter sw = _pathOut)
             {
-                foreach(string n in outWords)
+                foreach (string n in outWords)
                 {
-                    sw.WriteLine(skip+n);
+                    sw.WriteLine(skip + n);
                 }
             }
-                Console.ReadLine();
         }
-        
+
+        static void MethodTwo(StreamReader _pathIn, StreamWriter _pathOut)
+        {
+            string skip = " ";
+            int skipSymbol = 0;
+            List<string> outWords = new List<string>();
+            List<int> widhtText = new List<int>();
+
+            // Чтение файла
+            using (StreamReader sr = _pathIn)
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    if (skipSymbol < s.Length) skipSymbol = s.Length;
+                    widhtText.Add(s.Length/2);
+                    
+                    outWords.Add(s);
+                }
+            }
+
+            //ширина
+            for (int i=0;i<widhtText.Count; i++)
+            {
+                widhtText[i] = skipSymbol/2 - widhtText[i];
+            }
+            
+            //Количество пробелов
+            for (int i = 0; i < widhtText.Count; i++)
+            {
+                for (int j = 0; j < widhtText[i]; j++)
+                {
+                    outWords[i] = skip + outWords[i];
+                }
+            }
+
+            // Запись в файл File.CreateText(pathOut)
+            using (StreamWriter sw = _pathOut)
+            {
+                foreach (string n in outWords)
+                {
+                    sw.WriteLine( n);
+                }
+            }
+        }
     }
 }
