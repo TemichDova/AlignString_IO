cebce63cc72b293b57d88da86348c2073dfb0427
TemichDova <pirokar96@mail.ru>
2018-05-29 12:43:52 +0300

    Добавлена возможность выравнивание текста по правому и левому краю. Включена возможность изменять и создавать новые пути к файлам, а также изменение входящего файла. Есть возможность выбора выравнивание.

--- AlignString_IO/Program.cs
+++ AlignString_IO/Program.cs
@@ -2,29 +2,37 @@
 using System.Text;
 using System.IO;
 using System.Collections.Generic;
+using System.Diagnostics;
 
-namespace AlignString_IO
+
+namespace AlignString_IO 
 {
-    class Program
+    class Program 
     {
+        static WorkingTheFiles workingTheFile = new WorkingTheFiles();
+
         static void Main(string[] args)
         {
-            /*
-            string skip = "";
-            int skipSymbol = 0;
-            List<string> outWords = new List<string>();
-            */
-
             // Путь к файлам
-            string pathIn = @"..\..\..\in.txt";
-            string pathOut = @"..\..\..\out.txt";
+            string pathInFile = @"in.txt";
+            string pathOutFile = @"out.txt";
 
-            StreamReader _pathIn = new StreamReader(pathIn, Encoding.Default);
-            StreamWriter _pathOut = new StreamWriter(pathOut,false, Encoding.Default);
+            // var pathInReader = new StreamReader(pathIn, Encoding.Default);
+            // var pathOutWriter = new StreamWriter(pathOut,false, Encoding.Default);
+           
+            workingTheFile.InFileSearchPath(ref pathInFile);
+
+            workingTheFile.OutFileSearchPath(ref pathOutFile);
 
 
-            // MethodOne(_pathIn, _pathOut); первый алгоритм
-            MethodTwo(_pathIn, _pathOut);
+            Console.WriteLine("\nНачало процедуры");
+            Console.WriteLine("------------------------------");           
+            SelectionAlignTextInFile(pathInFile, pathOutFile);
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Процедура выполнена");
+
+            workingTheFile.OpenFile(pathOutFile);
 
             Console.ReadLine();
         }
@@ -61,52 +69,192 @@ namespace AlignString_IO
             }
         }
 
-        static void MethodTwo(StreamReader _pathIn, StreamWriter _pathOut)
+        static void MethodTwo(StreamReader _pathInReader, StreamWriter _pathOutWriter)
         {
             string skip = " ";
             int skipSymbol = 0;
             List<string> outWords = new List<string>();
             List<int> widhtText = new List<int>();
 
+            Console.WriteLine("Начало процедуры");
+
             // Чтение файла
-            using (StreamReader sr = _pathIn)
+            Console.WriteLine("Чтение файла");
+            using (StreamReader sr = _pathInReader)
             {
                 string s = "";
                 while ((s = sr.ReadLine()) != null)
                 {
-                    if (skipSymbol < s.Length) skipSymbol = s.Length;
-                    widhtText.Add(s.Length/2);
-                  
-                   
-                    outWords.Add(s);
+                    if (skipSymbol < s.Length)
+                    {
+                        skipSymbol = s.Length;
+                        widhtText.Add(skipSymbol/2-s.Length/2);
+                        outWords.Add(s);
+                        if (widhtText.Count - 1 != 0  && widhtText[widhtText.Count - 2] == widhtText[widhtText.Count - 1])
+                        {
+                            for (int i = 0; i < widhtText.Count; i++)
+                            {
+                                if (i != widhtText.Count - 1)
+                                {
+                                    widhtText[i] = skipSymbol / 2 - outWords[i].Length / 2;
+                                    
+                                    outWords[i] = skip + outWords[i];
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        widhtText.Add(skipSymbol / 2 - s.Length / 2);
+                        outWords.Add(s);
+                        for (int j = 0; j < widhtText[widhtText.Count-1]; j++)
+                        {
+                            outWords[outWords.Count-1] = skip + outWords[outWords.Count-1];
+                        }
+                    }
+                    
                 }
             }
-            
-            //ширина
-            for (int i=0;i<widhtText.Count; i++)
-            {
-                widhtText[i] = skipSymbol/2 - widhtText[i];
-            }
-            
-            //Количество пробелов
-            for (int i = 0; i < widhtText.Count; i++)
-            {
 
-                for (int j = 0; j < widhtText[i]; j++)
-                {
-                    outWords[i] = skip + outWords[i];
-                }
-            }
-
+            Console.WriteLine("Запись файла");
             // Запись в файл File.CreateText(pathOut)
-            using (StreamWriter sw = _pathOut)
-            {
-                foreach (string n in outWords)
+            using (StreamWriter sw = _pathOutWriter)
+            { 
+                for (int i=0;i<outWords.Count;i++)
                 {
-                    sw.Write( n+"\n");
+                    if (i != outWords.Count - 1)
+                    {
+                        sw.Write(outWords[i] + "\n");
+                    }
+                    else
+                    {
+                        sw.Write(outWords[i]);
+                    }
                 }
-                                
             }
         }
+
+        static void SelectionAlignTextInFile(string sourceFileName, string destinationFileName)
+        {
+            int indexSelectionAlignText = -1;
+            Console.WriteLine("Выберите метод выравнивание текста: \nПо левому краю - 0 \nПо правому краю - 1 \nПо центру - 2");
+
+            //Метка для goto
+            Restart:
+                if(indexSelectionAlignText != -1) Console.WriteLine("Вы ввели не верное значение, введите заного");
+                        
+            if (int.TryParse(Console.ReadLine(), out indexSelectionAlignText))
+            {
+                switch (indexSelectionAlignText)
+                {
+                    case 0:
+                        Console.WriteLine("\nВыравниение по левому краю");
+                        AlignTextInFile(sourceFileName, destinationFileName,indexSelectionAlignText);
+                        break;
+
+                    case 1:
+                        Console.WriteLine("\nВыравниение по правому краю");
+                        AlignTextInFile(sourceFileName, destinationFileName, indexSelectionAlignText);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nВыравниение по цетру");
+                        AlignTextInFile(sourceFileName, destinationFileName, indexSelectionAlignText);
+                        break;
+
+                    default:
+                        goto Restart;
+                }
+            }
+            else
+            {
+                goto Restart;
+            }
+
+        }
+
+        static void AlignTextInFile(string sourceFileName, string destinationFileName, int indexAlignText)
+        {
+            int maxWidthAlign = getMaxLineLenght(sourceFileName);
+            string[] linesText = LinesTextInFile(sourceFileName);
+
+            using (StreamWriter sw = new StreamWriter(destinationFileName, false, Encoding.Default))
+            {
+                for(int i= 0; i < linesText.Length; i++)
+                {
+                    if (linesText.Length - 1 != i)
+                    {
+                        sw.WriteLine(AligningToLine(linesText[i], maxWidthAlign,indexAlignText));
+                    }
+                    else
+                    {
+                        sw.Write(AligningToLine(linesText[i], maxWidthAlign,indexAlignText));
+                    }
+                }
+            }
+        }
+
+        static int getMaxLineLenght(string fileName)
+        {
+            int countSkipChar = 0;
+            
+            using (StreamReader sr = new StreamReader(fileName, Encoding.Default))
+            {
+                string readLine;
+                while ((readLine = sr.ReadLine()) != null)
+                {
+                    if (countSkipChar < readLine.Trim().Length ) countSkipChar = readLine.Length;
+                                      
+                }
+            }
+            return countSkipChar;
+        }
+
+        static string AligningToLine(string line, int width, int indexAlignText)
+        {
+
+            if (line == "")
+            {
+                return line;
+            }
+            
+
+            switch (indexAlignText)
+            {
+                //Left Align
+                case 0:
+                    return line.PadRight(line.Length);
+
+                //Right Align
+                case 1:
+                    return line.PadLeft(width);
+
+                //Center Align
+                case 2:
+                    return line.PadLeft(width / 2 - line.Length / 2 + line.Length);
+                default:
+                    return line;
+                                
+            }
+                        
+        }
+             
+
+        static string[] LinesTextInFile(string fileName)
+        {
+            List<string> lineText = new List<string>();
+
+            using (StreamReader sr = new StreamReader(fileName, Encoding.Default))
+            {
+                string readLine;
+                while ((readLine = sr.ReadLine()) != null)
+                {
+                    lineText.Add(readLine.Trim());
+                }
+            }
+            return lineText.ToArray();
+        }
+
+       
     }
 }
---
+++ AlignString_IO/WorkingTheFiles.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlignString_IO
+{
+    class WorkingTheFiles
+    {
+        
+        public void InFileSearchPath(ref string pathFile)
+        {
+            string categoryInFile = "Входящий";
+            //метка для goto
+            SearchPathFile:
+           
+            if (SearchPathFile(pathFile,categoryInFile))
+            {
+                Console.WriteLine("\nПродолжить работать в этом файле?: Yes (Y), No (N)");
+                
+                //Если null, то выдает ошибку. если значение 'nTEXT', то выдает true. 
+                char boolChar = Console.ReadLine()[0];
+                if (boolChar == 'N' || boolChar == 'n')
+                {
+                    Console.WriteLine("\nНовый путь:");
+                    pathFile = Console.ReadLine();
+                    NewPathInFile(pathFile);
+                    goto SearchPathFile;
+                }
+                else
+                {
+                    OpenFile(pathFile);
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("\nЗадать новый путь к входящиму файлу");
+                pathFile = Console.ReadLine();
+                NewPathInFile(pathFile);
+                goto SearchPathFile;
+            }
+        }
+
+        public void OutFileSearchPath(ref string pathFile)
+        {
+            string categoryOutFile = "Выходящий";
+            //метка для goto
+            SearchPathFile:
+
+            if (SearchPathFile(pathFile, categoryOutFile))
+            {
+                Console.WriteLine("\nПродолжить работать в этом файле?: Yes (Y), No (N)");
+                
+                //Если null, то выдает ошибку. если значение 'nTEXT', то выдает true. 
+                char boolChar = Console.ReadLine()[0];
+                if (boolChar == 'N' || boolChar == 'n')
+                {
+                    Console.WriteLine("\nНовый путь:");
+                    pathFile = Console.ReadLine();
+                    NewPathInFile(pathFile);
+                    goto SearchPathFile;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("\nЗадать новый путь к выходящиму файлу");
+                pathFile = Console.ReadLine();
+                NewPathInFile(pathFile);
+                goto SearchPathFile;
+            }
+        }
+
+        public bool SearchPathFile(string pathFile, string categoryIO)
+        {
+            var fileInfo = new FileInfo(pathFile);
+
+            if (File.Exists(pathFile))
+            {
+                Console.WriteLine("\nДоступен исходный {1} файл {0}", fileInfo.Name, categoryIO);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("\nФайл не найден "+pathFile);
+                return false;
+            }
+            
+        }
+
+        public void NewPathInFile(string newPath)
+        {
+            
+            try
+            {
+                using (FileStream fs = File.Create(newPath))
+                {
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Неверный путь к файлу");
+               
+            }
+            
+        }
+
+        public void NewWriteFile()
+        {
+            
+        }
+
+        public void OpenFile(string pathFile)
+        {
+            var fileInfo = new FileInfo(pathFile);
+            Console.WriteLine("Открыть {0} файл? Yes (Y), No (N)",fileInfo.Name);
+            char boolChar = Console.ReadLine()[0];
+            if (boolChar == 'Y' || boolChar == 'y')
+            {
+               
+                Process.Start(pathFile);
+            }
+           
+        }
+    }
+}
